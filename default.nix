9547620e280bdc2a5fe7fdff6901709950c67ef2
theNerd247 <noah.harvey247 gm>
2020-04-23 14:33:20 -0700

    adds nix derivation build

---
+++ default.nix
@@ -0,0 +1,32 @@
+{ pkgs ? import <nixpkgs> {}}:
+
+with pkgs;
+
+let
+  neuronGit = builtins.fetchGit 
+  { url = "https://github.com/srid/neuron"; 
+    ref = "master"; 
+  };
+
+  neuron = import neuronGit.outPath 
+  { inherit pkgs;
+    gitRev = neuronGit.shortRev; 
+  };
+in
+
+stdenv.mkDerivation
+{ name = "noah-zettelkasten";
+  buildInputs = [ neuron ];
+  src = ./.;
+  buildPhase = 
+    ''
+    mkdir -p site
+    neuron -d . rib -o site
+    '';
+
+  installPhase =
+    ''
+    mkdir -p $out
+    cp -r site/* $out/
+    '';
+}
