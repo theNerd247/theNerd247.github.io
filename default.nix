d03f543b7e1e63a0227f41cb9b72c6a4975698d5
theNerd247 <noah.harvey247 gm>
2020-04-29 16:41:44 -0700

    moving away from neuron to pandoc

--- default.nix
+++ default.nix
@@ -16,16 +16,27 @@ in
 
 stdenv.mkDerivation
 { name = "noah-zettelkasten";
-  buildInputs = [ neuron ];
+  buildInputs = [ pandoc ];
   src = ./.;
   buildPhase = 
     ''
-    ${neuron}/bin/neuron -d . rib -o ./docs
+    # ${neuron}/bin/neuron -d . rib -o ./docs
+    ${pandoc}/bin/pandoc\
+        --resource-path=./static\
+        -t html\
+        -f markdown\
+        --standalone\
+        --table-of-contents\
+        --output=./index.html\
+        --file-scope\
+        ./*.md
     '';
 
   installPhase =
     ''
     mkdir -p $out
-    cp -r docs/* $out/
+    # cp -r docs/* $out/
+    cp ./index.html $out/
+    cp -r ./static $out/static
     '';
 }
--- index.md
+++
@@ -1,9 +0,0 @@
----
-title: Overview
----
-
-[View it on GitHub](https://github.com/theNerd247/zettelkasten)
-
-Lambda Calculus
-  * <2016404> 
-  * <2016407>
