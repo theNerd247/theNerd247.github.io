8eab751999069a7b61dd81d755fa89f691ef6b1d
theNerd247 <noah.harvey247 gm>
2020-08-05 13:45:15 -0400

    adds missing copyJoin

---
+++ copyJoin.nix
@@ -0,0 +1,12 @@
+pkgs: name: paths:
+pkgs.runCommand name { passAsFile = [ "paths" ]; inherit paths;}
+  ''
+  mkdir -p $out
+  for i in $(cat $pathsPath); do
+    if [[ -d $i ]]; then
+      cp $i/* $out/
+    else
+      cp $i $out/$(stripHash $i)
+    fi
+  done
+  ''
