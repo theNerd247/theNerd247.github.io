bfe9a8cad08933270375f5abbd614db29b14f77d
theNerd247 <noah.harvey247 gm>
2020-08-17 14:34:15 -0400

    working on modus ponens draft

--- contents/making-a-sandwich.nix
+++ contents/making-a-sandwich.nix
@@ -7,38 +7,76 @@
 
 Here's a question:
 
-> If I give you the recipe for making a sandwich and all of the recipe's
-> requirements have I given you a sandwich?[^1]
+> ''
+(conix.text ["prem1"] ''If I give you the recipe for making a sandwich'')
+" and "
+(conix.text ["prem2"] ''all of its ingredients, cookware, etc.'') 
+(conix.text ["conc"] ''Can I assume that at somepoint in time a sandwich exists?'')
+''
 
-If I said yes, the you'd call me absurd - and rightly so. Having knowledge
-and materials doesn't imply that the job has been done.
 
-But what if we lived in a world where this was true? That is, what if the
-existence of instructions and materials implied the existence of results? Then
-the answer to the above question would be yes! (And I would have gotten into
-less trouble with my parent's as a kid).
+To be clear, I'm not asking if you are lazy or eager in your work ethics.  The
+answer to this question depends on whether I can assume you know what to do
+with both ingredients and recipe and that I will get back as having a
+sandwich.
 
-Now in the real world things like time, human will, and God get prevent us from
-being able to make such claims. But programming isn't the real world - it's the
-world of the mind where most things are possible (so long as they are logically
-consistent). Every programming language must answer yes to the above question.
+The answer to this question is at the heart of computer science, functional
+programming, and mathematics.
 
 ## Modus Ponens
 
-Our sandwich question can be formally stated in mathematics. Let 
+Let's restate the question in more formal terms. I'll use capital letters and
+symbols to represent the logical statements about sandwiches above.
 
-  * Let `P` represent "the requirements"
-  * Let `P -> Q` represent "the recipe exists" (and also to mean "If the requirements exist then the sandwich exists")
-Modus Ponens says:
+English | Symbols
+--- | ---
+"''(conix.textOf [ "posts" "making-a-sandwich" "prem1" ])''" | `P -> Q`
+"and"                                                        | "&"
+"''(conix.textOf [ "posts" "making-a-sandwich" "prem2" ])''" | `P`
+"''(conix.textOf [ "posts" "making-a-sandwich" "conc"  ])''" | `Q`
 
+To write the entire statement using only symbols looks like:
+
+```
+(P -> Q) & P -> Q
+```
+In mathematical logic this statement (called "modus ponens") is a true
+statement. To rephrase it in general English: "If `(P -> Q)` exists and `P`
+exists then that implies that `Q` exists". 
+
+This gives us the answer to our original question: yes, the existence of
+recipes and their matching ingredients implies that the sandwich exists if
+modus ponens holds.
+
+## So What? 
+
+Programmers abstract their programs using functions. Large and complex
+processes can be summarized with a single word. In short, functions are
+recipes and their arguments are ingredients. Let's restate the above in
+terms of a function in a few languages. 
+
+1. Haskell:
+  ```haskell
+  apply :: ((a -> b), a) -> b
+  apply (f, a) = f a
   ```
-  (P -> Q) -> P -> Q
+1. Typescript
+  ```typescript
+  function apply<A,B>(f, a) : B { return f (a); }
   ```
-In English: If the statement "If P is true then Q is true" is true and "P is true" then "Q is true".
+1. C++
+  ```c++
+  template<A,B> B apply(f Func<A,B>, a A) {
+    return f(a);
+  }
+  ```
 
-The Curry-Howard isomorphism
+The statement `(P -> Q) & P -> Q` is a function called apply. The Haskell
+version's type looks very similar to the math form. If you happen to know all
+three languages you'll notice something: they are all just calling a function
+with the given arguments. 
 
-## P
+In fact, there's something called the Curry-Howard isomorphism that says that
+calling a function is the same thing as pair a function with its arguments.
 
-[^1]: By given I mean: will we end up with a sandwhich after some period of time?
 ''])
