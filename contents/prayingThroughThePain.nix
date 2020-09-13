c266d6e9e5890b13373e75b50206ae8ace9f379b
theNerd247 <noah.harvey247 gm>
2020-09-13 11:55:24 -0400

    adds contents/prayingThroughThePain.nix

---
+++ contents/prayingThroughThePain.nix
@@ -0,0 +1,160 @@
+conix: { sermons.prayingThroughThePain = with conix.lib;
+let 
+  ref = conix.sermons.prayingThroughThePain.ref 
+in 
+  texts [
+''
+# ''(label "title" "Praying Through The Pain")''
+
+# Intro
+
+Christians are heading down the highway at 70 miles an hour in life and know
+there's something that needs tending but don't stop to address it.
+
+God uses pain to get our attention to address what He wants to change us.
+
+How can my pain connect me better with God? How can I go to God in prayer to
+connect with His peace in my life?
+
+''(label "ref" "Romans 8")'':14-17
+
+1. Leading
+2. Assuring us of our relationship
+3. Acting as our assurance of our glorification
+
+Like sonar we need something to prevent us from running into walls.
+
+${ref}:26-27
+
+Before we come to Christ we are upside down; salvation rights us. But our world
+is still upsidedown; thus collateral damage and difficulty remains in our
+lives. Sin still resides in us.
+
+${ref}:18 we still go through hardships and suffering. However, the assumption
+is that we pray.
+
+_How can my pain connect me better with God?_
+
+# Let The Pain Summon You To More Prayer
+
+Have a mindset that you are going to take it to the Lord in prayer.
+
+Increase your quiet time! Make more time for prayer.
+
+Often we just vent to others. Christian, take it to the Lord in prayer first!
+If your first thought is to fix it, push your next to be to take it to the Lord
+in prayer. This is a vital resource in the Christian life.
+
+Ps 23 - We say "God I don't want to walk through the valley of death how about
+taking me another way?" No, David realises that he must go through the valley.
+God doesn't always calm the storm - nevertheless he will bring some kind of
+deliverance to those who call upon God.
+
+Ps 34:15-22
+
+Emotionally hard times will hit - we rush to fix the problem or turn to someone
+to talk to.
+
+Can you drop to your knees and cry out to God: "I give up...what do you want me
+to do?" Can you run to God in prayer? _Don't leave that privilege on the
+table_.
+
+> Oh what peace we often forfeit 
+> Oh what needless pain we bear. 
+> All because we do not carry 
+>   everything to God in prayer.
+
+Spend extra time with your heavenly father.
+
+Like in war - the radio man is the biggest target; it prevents us from
+communicating for reinforcements.
+
+${ref}:26
+
+help
+: sumantilambanomai
+: To come to the aid
+: to share the load
+
+weakness
+: not knowing for what to pray or how to pray
+
+Johari Window
+
+''(table ["", ""]
+ [[ "open self"  , "blind self"]
+  [ "hidden self", "uknown self"]
+])''
+
+# Recognize Your Weakness 
+
+${ref}:26
+
+State the problem to God - not the solution.
+
+> we do not know what to pray
+> NJKV
+
+Greek NT says:
+
+> We do not konw "the what" to pray for. 
+
+Thus there is something specific.
+
+> We do not know what to pray for as it is necessary.
+
+We need to know what to pray for specifically. We pray because we are hurthing.
+"God I have this pain and I want you to remove that pain".  But we miss out on
+the important lesson we are being taught in our lives.
+
+We shape our prayer requests more based on how we feel than God's will.
+
+Why recognize our weakness? in ${ref}:26 Paul is asking us to go to God and ask
+Him to show us his will.
+
+Did Paul not know God's will? 
+
+2 Cor. 12 
+
+${ref}:26b The Spirit intercedes on behalf of the believer.
+
+Imagine seeing a man in trouble with the cops as you drive by. Knowing him you
+stop and defend him; getting him out of trouble.
+
+## Mis-Interpretations of ${ref}:26
+
+* As an argument for the gift of tongues
+* A profound judgement such that it's inexpressable. They use 2 Cor 9:15, 12:4,
+  1 Pe 1:8 to argue for this.
+* The spirit taking us to higher and holier thoughts to the point the only expression
+  is groanings and sighings.
+
+inexpressable
+: wordless
+
+Other places of intercession 
+
+  * 1 John 2:1
+  * John 17?
+
+The defence attorney intercedes for you by telling you what to say before the
+judge.
+
+Phil 2:12b here is a reference of how God directs us.
+
+The Spirit's intercession is God revealing his will to us.
+
+Like a child sitting in the parent's lap and driving. We only steer
+
+# Trust Your Divine Advocate To Guide You
+
+${ref}:27
+
+# Application
+
+* Pain in life is not all bad to God
+* Remember when you are weak God is near
+* Believe that your divine advocate will guide you, if you surrender to Him.
+  Lord I am not going to get up until you speak to me.
+
+''];}
