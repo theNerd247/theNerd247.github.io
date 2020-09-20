42d0049e9ec1ce7127f8772e36351b6eacb585bc
theNerd247 <noah.harvey247 gm>
2020-09-20 11:50:55 -0400

    adds contents/findingGoodInBad.nix

---
+++ contents/findingGoodInBad.nix
@@ -0,0 +1,121 @@
+conix: { sermons.findingGoodInBad = with conix.lib; 
+let
+  ref = "Romans 8";
+in
+
+texts [
+
+''# ''(label "title" "FInding Good In The Bad")''
+
+## Intro
+
+Cliff Barrells story of meeting Billy Graham. What he thought was a flop
+turned out to be God's providencial hand.
+
+${ref}:28 - "and"
+
+### First Key: "we know"
+
+in present tense - habitual action. "And we are knowing".
+
+"know"
+: gnosko - a beginning knowledge
+: epignosis - places emphasis on top of. "Knowledge ontop of knowledge".
+Knowledge + an experience that drives that knowledge home.
+: oida - Full, complete, and absolute knowledge. Includes prior two definitions
+
+"We have a continuing; absolute knowledge that we can rest in".
+
+You learn stuff - but then it becomes an experiencial truth which allows you to
+rest. Paul puts this statement in the form of an assumption. We should have
+this knowledge.
+
+## Comprehend Who Directs All Things
+
+together
+: synergeo
+: synergism - working of elements together to produce an effect greater than
+and possibly different. Example: table salt: NaCl. 2 poisions = 1 inert
+compound.
+
+> All things work together for good to them who love God
+> - KJV
+
+This language implies the wrong idea. Not everything is for the good.
+
+_God causes all things..._
+
+
+The universe doesn't work automatically - it is still under God's control.
+Job 26:8-9, Col 1:16-17.
+
+> Providence is the hand of God in the glove of history.
+
+In ${ref}:28 God is sovereign. He exercises His perogative to exercise whatever
+He wants.
+
+Job 23:13
+
+There is no such thing as chance, luck, or mistakes. 
+
+How do we translate this? I need to have complete confidence of God's complete
+control over my life.  This passage is not stating a larger benefit exists for
+every negative circumstance.
+
+## Comprehend What Good Means
+
+good
+: His purposes for our lives.
+
+${ref}:29 "For those whom he foreknew": good means conforming to the image of
+Christ.
+
+The first effect of a problem is a shock - a realization of a problem. We are
+ready to pay attention to a problem. What is dangerous is when everything is
+going smoothly. Trials put our eyes back onto the Lord. One doesn't go to the
+doctor until there is pain in their life.
+
+It is the storm that cause us to look tot he light house of God's Word.
+
+The weaker I am the more the power of God can be released. God puts pain into
+our lives to get us to walk closer to Him and correct the sin in our lives.
+
+Genesis 50:19-20
+
+## Comprehend Who Are Those Who Love God
+
+"those who love God" - this doesn't mean ALL believers. It's not a subjective
+statement. You cannot just say "Yeah I think I love God".
+
+Do you love God? The real God - not the god of your imagination or feelings. We
+all go around in our culture and avoid this book [the Bible].
+
+If I don't love the God of this book then I love the God of my imagination.
+
+Psalm 50:21 - God said "you thought I was like you but you are wrong". We
+project our ideas values and preferences on God.
+
+There is emotion in worship - but it cannot be just that. 
+
+John 14:21 - those who love God keep His commandments. Love is always practical
+- it is always willing to sacrifice it for Him.
+
+Daniel and the lions den. _Our love is proven through trials more than what we
+feel and think._ 
+
+Should we accept only good from God and not the bad? Though He should slay me I
+will trust Him!
+
+## Application
+
+Comprehend:
+
+  * who directs "all things"
+  * what "good" means
+  * who "are those who love God".
+
+* Be sure you have a Biblical love
+* Trust in God's management of All things
+
+''
+];}
