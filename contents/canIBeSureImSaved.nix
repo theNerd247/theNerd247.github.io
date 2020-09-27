c74b7444702a98b21a69663438506f1545342094
theNerd247 <noah.harvey247 gm>
2020-09-27 11:56:56 -0400

    adds contents/canIBeSureImSaved.nix

---
+++ contents/canIBeSureImSaved.nix
@@ -0,0 +1,117 @@
+conix: { sermons.canIBeSureImSaved = with conix.lib; 
+let
+  ref = "Romans 8";
+in
+
+texts [ 
+
+''# ''(label "title" "Can I Be Sure I'm Saved?")''
+
+# Intro
+
+${ref}:28-30
+
+How do I know that I'm saved?
+
+Others fear that they have done something that causes us to lose our salvation
+and live on eggshells.
+
+v31.
+
+"these things" refers to the same "things" in v32.
+
+# I am assured by my love of God  
+
+"those who love God" - this is not a warm fuzzy thing we get (not a spiritual
+high).  This is evidence (proof) that we are saved. 1 John 4:19. God first
+loved us, and then we loved Him. If I'm obedient to Him then I want to please
+Him and that shows that I love Him. 
+
+2 types of love in marriage:
+
+  * One spouse is conditional in their love and the other constantly attempts
+    to gain the approval of the other person.
+  * The other type of love is the opposite.
+
+${ref}:28,30
+
+# Am I assured by God's call
+
+There is indeed a call to ministry - however this is not the calling noted in
+v30.
+
+2 Peter 3:9
+
+Universalism 
+: everyone will eventually come to Christ.
+
+Can we honestly I am what I am by God's grace? Are there things in my life that
+can only be explained by the supernatural?
+
+What are your interests these days? Is it to know Christ? Is it to participate
+in the growth of God's kingdom?
+
+What has made you what you are? Are you convicted by sin? Conviction is like
+an annoying smoke alarm. 1 Thes 2:13
+
+# I am assured by God's purpose for me
+
+"foreknew"
+: doesn't mean predetermine, predestine, or foreordination
+: pros (before) * ginosko (knowledge)
+
+"predestine"
+: to markout set a boundary
+
+God, before the foundation of the world, has marked out a plan of redemption
+for us. For example, a soccer coach already determines what plays and team
+movement is to occur and so he sets up practices to direct the team to do this.
+
+${ref}:29 - Jesus was the first born of several brothers. We are conformed to
+the image of Christ because we are brothers; we have the same DNA.
+
+(dotDigraph "inChrist" ''
+
+Christ -> "Man In Christ +R" [label "Imputed righteosness"]
+Christ <- "Man In Adam -R"   [label "Forgiveness"]
+
+'')
+
+${ref}:30
+
+# I am assured by my justification
+
+3 things God could have done to address sin:
+
+  * issue verdict leaving us hopeless (condem all)
+  * comprimise justice and lower the standard
+  * provide a means to forgive sinners
+
+Romans 3:21-22
+
+God doesn't grade our righteousness on a curve - infact there isn't even a
+scale: it's a binary scale.
+
+2 Cor 5:21
+
+Justification doesn't mean "not guilty" but more: you are completely righteous
+before God.
+
+# Iam assured by my glorification
+
+${ref}:30 - you are glorified. 
+
+In ${ref}:18,21,23 Paul is talking about what will happen to us during the
+millenial.
+
+But in ${ref}:30 is a timeless statement - in God's plan it is already
+considered done (modus ponens?).
+
+# Application
+
+* Go on facts not feelings
+* This week think on this: Christ has a lot invested in you.... He is not
+going to give up on you now. There's nothing you can do to make Him displeased.
+
+''
+];}
