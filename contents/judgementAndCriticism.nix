106b0061b7aaf2455f3a2d5007f530b6023197fc
theNerd247 <noah.harvey247 gm>
2020-10-04 12:00:21 -0400

    adds contents/judgementAndCriticism.nix

---
+++ contents/judgementAndCriticism.nix
@@ -0,0 +1,175 @@
+conix: { judgementAndCriticism = with conix.lib; 
+let
+  ref = "Romans 8"
+in
+
+texts [
+
+''# ''(label "When You Experience Judgement and Criticism")''
+
+# Intro
+
+How do you feel when others oppose, criticise, or threaten you? Anger? Fear?
+Rejected? Worthless?
+
+Signifigant other x rejection (percieved / real) = anger and/or depression
+
+depression = anger turned inward
+
+What transpires in a person's psychy is amazing. People don't disagree and
+simply move on.  There is a lot of pain that is left behind.
+
+Remember that while this is the most encouraging passage in scripture we, as
+Christians, will and do suffer.
+
+Ronans 12:1-2 we need to have a biblical perspective in light of suffering.
+
+Implicit in this passage is:
+
+  * opposition
+  * criticism
+  * judgement
+
+these are where our emotions are most vulnerable and affected.  How should I
+respond to these?
+
+${ref}:31 
+
+"What then"
+: contextual tieback
+: in this case the context is ${ref}:29-30.
+
+This is bottom line theology that should be topline priority.
+
+* ${ref}:1
+* ${ref}:35
+
+${ref}:32
+
+"did not withhold"
+: Same language used when God was speaking of Abraham (Genesis 22:12)
+
+Abraham loved God so much that He didn't withhold his only son. In the same manner,
+so does God treat us - He loved us so much that He didn't
+
+# When Opposition Arises Be Confident
+
+In what? That He will take care of us.
+
+${ref}:32 
+
+"all things"
+: What God has promised to us in Christ
+: God providing for all our needs
+
+_What are those needs?_ Only physical? If God took care of our greatest need
+will he not provide everything else? If a husband gives the diamond ring will
+he not also provide the box if asked?
+
+Since we've received this wonderful gift and salvation
+
+2 Peter 1:2-3
+
+What if I sin? Will God not re-nig and remove His love for us? Doesn't He want us
+to come back to Him and will provide a way to restore fellowship? 
+
+"freely give"
+: charis
+: charizomai
+: To be given a gift that is underserved.
+
+${ref}:33
+
+"elect"
+: those who have trusted Christ
+
+## God is The One Who Justifies
+
+God is the one seated as a judge. When you are criticized it is not the person
+
+_But isn't the criticism valid? Do we dismiss what is said? Maybe it is we
+have to emotionally detach from the situation / not allow feelings of failure
+creep up?_
+
+Romans 4:2
+
+"if"
+: not case
+: This shows that man is not saved by what He does
+
+When we're dependent up on Christ we are perfect. Our flaws are covered and
+not held against us. 
+
+# When Your Enemies Judge You Be Fearless
+
+God values you and accepts you. You have worth, value, and are wanted.
+
+This is justificiation; the gift of worth without any merit.
+
+_God I don't believe this. Help my unbelief_.
+
+You don't have to fear what people think of you.
+
+Thoe who are marinated in God's love do not sell out to win the love of others
+(even God, because they know they have it).
+
+These verses give no room for fear. Let us not get to heaven and be embarassed 
+that we did not trust a fully trustworthy God.
+
+_How do we handle the shame of not trusting God?_
+
+${ref}:34
+
+Who is the one who can keep on condemning you?
+
+Micah 6:8 - be careful about listening to or speaking gossip for this condemns
+others.
+
+1 John 2:1, Heb 7:25
+
+It's like a team golf game. The best score out of the team is the teams score.
+
+# When Your Enemies Judge You Be Bold
+
+Satan/others love to bring up the past. Many people cannot let go of the past,
+even though they have put their lives back together.
+
+Rev 12:10
+
+_Who is our enemy?_
+
+"Do you know what he did yesterday?! He calls himself a Christian but look at
+what he does!? Can you believe it?". Christ replies, "Here is my blood, he has
+been forgiven".
+
+Zech 3:1-5; this is a picture of justification
+
+"angel of the Lord"
+: Pre-incarnate Christ
+
+"filthy garments"
+: a picture of sin
+
+Now about those golf holes?
+
+1. Resisting temptation when in your youth? Jesus hit a hole in one.
+1. Pressure to conform but to say something for God's honor? Jesus hit a hole in one.
+1. When you had righteous anger but kept silent? Jesus hit a hole in one.
+1. When you should have kept your mouth shut and looked the fool? Jesus hit a hole in one.
+
+Remember when playing team golf everyone gets a trophy. "Hey you didn't do so well! You don't deserve
+that trophy!" - but the one who hit the perfect score says "My scorecard is his score card"
+
+* be confident
+* be fearless
+* be bold.
+
+# Application
+
+* Remember God is pulling for you. Just like parents are for their kid when performing
+* Nobody can nullify what God has done for you and says about you.
+* Do not live in fear
+* 
+
+''
+];}
