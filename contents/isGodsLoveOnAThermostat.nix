571ed05161a45eb9bda64e574be1be601f67fc8d
theNerd247 <noah.harvey247 gm>
2020-10-11 11:56:32 -0400

    adds contents/isGodsLoveOnAThermostat.nix

---
+++ contents/isGodsLoveOnAThermostat.nix
@@ -0,0 +1,192 @@
+conix: { isGodsLoveOnAThermostat = with conix.lib; 
+
+let
+  ref = "Romans 8";
+in
+
+texts [
+
+''# ''(label "title" "Is GO'ds Love On A Thermostat?")''
+
+# Intro
+
+Have you ever had anyone say to you "Hey, you are skating on thin ice!".
+
+As adults we've all experienced the feeling that we're on thin ice. We
+prefer to say it like:
+  
+  * My marriage is failing
+  * my work situation is disintigrating
+  * the bottom is falling out of my financial life
+
+We all at times struggle with uncertainty.
+
+However, God's love for us is certain. That truth is biblically portrayed in 
+the Psalms:
+
+> "God is my refuge and my fortress. He is a shelter for me"
+
+God's love is like an aircraft carrier in our life. It gives us incomparable peace
+and security in our lives. 
+
+Proverbs 20:6
+
+There are a lot of people who talk about their loyalty but rarely are there
+those who will keep their commitment.
+
+God's unchanging nature makes it impossible for Him to ever give us insecurity.
+
+# What am I to Think about God's love during adversity?
+
+${ref}:35
+
+This chapter ends the first doctrinal section of the new testament.
+
+"love of Christ"
+: in this context it is synonymous to our salvation
+: Or "What can separate us from our salvation?"
+
+separate
+: chorizo
+: country, land, region, or a field
+: "Can anyone put a country between us and God's salvation?"
+
+tribulation
+: speaks of pressure
+: Offiliated when they want to punish someone by picking up a huge boulder and
+  put it on the chest of the person.
+
+distress
+: "stenochoria"
+: a tight squeeze
+: 2 Cor 4:9
+
+persecution
+: diogmos
+: the idea of one being chased
+
+nakedness
+: not nudity, but being so poor that they don't have the money to purchace
+clothes to keep warm in winter.
+
+peril
+: kindynos
+: in danger or at risk
+: 2 Cor 11:26
+
+
+Can any of the above separate us from God's salvation for us? ${ref}:37.
+Absolutely not!!!
+
+Jer 31:3
+
+Paul re-assures us of God's love ${ref}:36 by referencing Psalm 44:22. He 
+is saying that God's people should't be surprised that we will suffer.
+
+# ''(capcase "Adversity can be a Sign of God's salvation not rejection")''
+
+2 Tim 3:12
+
+"In Christ Jesus"
+
+1 Pe 4:12-14 
+
+revelation 
+: Christ's return during the millenium
+
+# ''(capscase "God has made you a super conquerer")''
+
+${ref}:37
+
+"but" 
+: Pay attention! Here's the great news!
+
+conquerer
+: nikao
+: victory
+: noun form is nike'
+
+huper
+: lit. "ontop of"
+
+"more than conquers"
+: hupernikao
+: super conquers in Christ
+
+loved
+: in aorist tense (a point in time)
+: This is pointing right back to the cross.
+
+Christ's victory on the cross ensures our victory in life. When we have a trial
+often we get and stay discouraged. 1 Cor 10:13. God will provide a way to
+escape. 
+
+Trials shouldn't be walking over us; we should be walking over our trials.
+
+We get into a pull of gravity and we see ourselves as loosers. "I am a victor!
+I don't have to give into this". 
+
+"God I know this feeling isn't true. And I am holding onto your promises and I
+trust that you will get me through this".
+
+${ref}:38: these are theats in the spiritual world.
+
+conviced
+: perfect tense
+: I have become at a point in time to remain strongly conviced.
+
+2 Pe 1:3
+
+knowledge
+: experiencial knowledge
+: The more I live by the word and experience His truth in my life I am conviced
+that nothing can separate me.
+
+Hymn: _It is well with my soul_: "Teach me to say it is well with my soul" was
+changed to "Teach me to know it is well with my soul".
+
+Knowledge is the instrument panel on that spiritual airplane that gets us
+through the opaque clouds.
+
+# ''(capscase "God wants you to live with confidence, not fear and anxiety.")''
+
+${ref}:39
+
+"In christ jesus our Lord"
+: this is packed with theological implications.
+
+''(dotgraph "inChrist" ''
+digraph {
+  Christian -> Christ [label="in"]
+  Christ -> God [label="in"]
+}
+
+'')''
+
+John 10:28
+
+I give eternal life to them (believers).
+
+never
+: a double negative in the greek
+: repetition is compounding.
+
+perish
+: subjunctive mood meaning they don't evean have the potential of parishing
+: middle voice the subject acts on itself. They don't even have the potential
+to perish in and of themselves.
+
+Could it be any clear? We are in Christ and hidden in Christ.
+
+What if I even set myself out of the reach of God?
+
+# Application
+
+1. adversity is a sign of salvation
+1. I am a super conquerer - not a looser
+1. God wants you to live with confidence - not fear and anxiety.
+
+__REST IN GOD'S CONFIDENCE_
+
+
+''];}
