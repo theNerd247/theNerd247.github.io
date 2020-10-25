80d3945bee7e92845b063f966478726acd203d03
theNerd247 <noah.harvey247 gm>
2020-10-25 11:55:07 -0400

    adds contents/theHolySpiritComforter.nix

---
+++ contents/theHolySpiritComforter.nix
@@ -0,0 +1,114 @@
+x: with x; [
+
+''# ''{ title = "The Holy Spirit Our Comforter"; }''
+
+## Intro
+
+You are better off with Christ now, yesterday, tomorrow and for eternity.  You
+know of His love, joy, and peace, and eternity to come.  Without Christ there
+is uncertainty, no purpose, no peace, and eternal suffering.
+
+What if someone said "You are better off without Christ"? This was said by
+Christ himself. 
+
+John 16:7:advantage
+: profit
+
+Christ left in order to send us His own Spirit to each of us. His Spirit is not
+bound by space or time and thus can be omnipresent. Like a heart transplant, we
+have the heart of Christ beating in us. 
+
+2 Peter 1:4 - God has planted his Spirit in us. The more you feast on God's
+promises and His Word then the more you begin to partake and activate the
+divine nature in your life.
+
+John 14:17:know
+: experiential knowledge
+: or "you know by experience"
+
+Herein lies the problem: 2 extremes today. 
+
+1. An over emphasis on the HS to the
+  detriment of the person of Christ. We tend to equate the H.S with emotions.
+  The HS is not isomorphic to feelings. Yes, He may affect our feelings, but
+  not always.
+
+2. Many do not give more then lip service to the HS in their lives. There are
+many Church goers who have not experienced God in their lives. The benchmark of
+success has become more about attendance and entertainment. It filled our
+churches with self focused consumers rather than the self-sacrificing tombs.
+
+Many believers live powerless lives.
+
+## Recap
+
+The HS renews and restores the believer's life so that He may change the
+world. You are not just here to enjoy the good life.
+
+## How will the Holy Spirit Guide Our lives?
+
+2 words are the key to understanding this passage:
+
+John 16:8:convict
+: to convince or refute
+
+John 16:7:Helper
+: parakletos
+: one who stands beside
+: to summon exhort encourage, comfort
+
+A councilor is one who comes alongside another to convince them of truth and
+walk beside the person they council. However, many read this and think the HS's
+purpose is to come down on the unbelievers to convict them of their sin.
+
+The reason for many Christian's pain is they are living life outside of
+reality. The nature of sin is to ignore the one in charge of sin. `John 16:9 -
+sin = unbelief.`
+
+1. The help believers make known man's greatest sin: that of not believing in
+Christ. John 3:18. The purpose of convincing is not to condemn the world in
+self righteousness but to stand alongside them and say - in gentle spirit -
+"Hey, let me show you what God has done in my life." They are not the enemy,
+but a victim of the enemy (Satan). When you go the movies they show you
+previews to get you to see them. As a believer you are a preview into the
+eternal kingdom of God. Being a witness is a great privilege but also a great
+responsibility.
+
+1. To help believers reveal the righteousness of Christ. John 16:10 - the
+righteousness of God. We speak of who Christ is. 
+
+  sin
+  : harmatia
+  : to miss the mark
+  : an arrow missing the bullseye
+
+  God does not grade on a curve. Isaiah 64:6
+
+''(indent 2 (table 
+  [ "Man" "Christ" ]
+  [["-R" "R +INF"]
+   ["+R" "-R & R +INF"]
+  ]
+))''
+
+  Romans 4 "He who knew no sin..."
+
+1. We are to help believers (by the aid of the HS) to make known the judgement
+that is to come.
+  John 16:11:judged
+  : perfect tensed
+  : at the Cross Satan was judged and continues to be judged.
+
+  Satan is still hanging around because the sentence hasn't been carried out
+  yet. There is a judgement to come because Satan has been judged.
+
+## Application
+
+1. A primary purpose of the believer is to help the unbeliever see Christ
+1. The HS will bring conviction in our lives
+1. The successful Christian in this world depends on the HS in our lives.
+
+Going back to the heart transplant analogy: _God says "I want to hear my heart
+beating strongly in you"_.
+
+'']
