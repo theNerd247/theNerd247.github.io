394e2fa0bd9beeeb1c0d9fc4cc19e4fcba23d904
theNerd247 <noah.harvey247 gm>
2020-11-01 11:58:21 -0500

    adds sermons/aNewKindOfLife.nix

---
+++ sermons/aNewKindOfLife.nix
@@ -0,0 +1,160 @@
+x: with x; [
+
+"# "{ title = "A New Kind Of Life"; }''
+
+''{ref = "Eph 5";}'':18
+
+# Intro
+
+We have resident in each of us God's heart, we have all the resources
+we need and yet believers live as if they are living on empty.
+
+Have you lost hope that your life (Christian) is not what it is what
+you want it to be? Do you struggle with the same sin or worthless feeling
+with no avail? 
+
+Yes they may believe God will help them but what does that really mean? 
+We believe 
+
+> "God helps those who help themselves"
+
+There are little difference between the habits of the believer and the secular
+man. Guilt is usually the main motivator for obedience and thus is usually
+short lived. One of the great dangers of the Christian life is that we grow
+complacent and we lose the passion for Jesus! Our greatest fear is that we become
+a wicked old man - calloused and unpleasant.
+
+## We are Commanded To Be Filled
+
+''(r data.ref)'':14
+: quote out of OT 
+
+death
+: separated from God
+: Practically - a ho-hum complacent Christian life
+
+''(r data.ref)'':15-16 be:
+
+  * wise
+  * redeem the time
+"Therefore [to do the above] do not be foolish _but_ understand what the will
+of the LORD is."
+
+''(r data.ref)'':18 This is a command! Each believer is to be filled with the
+Spirit.  He's not saying "This would be a great idea if you would just let the
+HS control you".
+
+_God is not into suggestions but commands_.
+
+There is no command to be baptized, sealed, or indwelled by the HS. 
+
+filled
+: present tense (keep on being filled)
+: in plural (Y'all be filled) it is meant for all Christians
+: Grk: "pleroo"
+: power or control that is to be had. Often spoken of the wind filling the
+  sails of a ship.
+: lit. filling a cup
+: see John 16:6, Luke 4:28, Acts 13:45
+
+## It is meant for all believers
+
+You cannot see the HS but you can see the effects of the HS. He is like
+electricity - He is always there, but you have to tap into Him. Without
+the Spirit controlling us life can become a hassle. 
+
+How often do we look at other believers and say "What a spiritual giant!"
+why can't I be like that? When the HS fills a particular believer He manifests
+His works in a way that is tied closely with that person's natural gifts
+and personality/temperament.
+
+''(digraph "In Christ" ''
+
+Hell -> cross [label="salvation"]
+Cross -> "In Christ" [label="Position"]
+Cross -> "Filling of HS" [label="Fellowship"]
+
+"Filling of HS" -> "OOF" [label="sin"]
+"OOF" -> "Filling of HS" -> [label="confession"]
+
+'')''
+
+Ministries of HS:
+
+  * Indwelt
+  * Sealed
+  * baptized
+One can resist the filling ministry of the HS, but you cannot do the filling
+yourself. When we allow the HS to control us we can do things we never
+would have imagined.
+
+You cannot fill something that is already filled. If you already are filling
+your life with things that soothe your deep longing the HS cannot fill within
+you. Sadly Christians too often are trying to fill their life with things
+that bring no long term solution.
+
+Because of the present tense.
+
+## This is a continuous experience
+
+The filling of the HS. Life depletes that ministry in our lives.
+
+Even though it is a continuous action, do not believe that because you are not
+being filled then He leaves you. "If I've done such and such horrible thing
+then He would leave me". You __must DAILY activate the filling of the HS__.
+
+## It Is A Matter of Control
+
+This isn't just an indwelling, but a controlling. A drunk person is controlled
+by alcohol, a HS filled person is controlled by the HS. 
+
+drunk
+: riotousness or rowdiness such that almost a different person is exhibited.
+
+The man who is filled acts in ways that are unnatural to Him. _The controlling 
+of the HS removes all excuses in one's life_. 
+
+  * my personality - don't blame your sin nature on God.
+  * this is how I was raised - God can change you
+  * I've always been this way - you have not known fully what it is to be guided fully.
+
+God plants His heart into our lives. Instead of spending our time and energy to
+change ourselves we should spend our times learning how to be filled. A drunk need only to
+take a drink to be drunk
+
+
+## How do I activate the Spirit's filling?
+
+* Be totally dependent upon God: embrace the essential element of total
+dependence. John 15: apart from me you can do Nothing.
+
+The spirit filled life begins when we are absolutely convinced we can do
+nothing apart from the HS.  When we reveal we are completely hopeless! Without
+meaning to Christians live without God's spirit in their daily lives. When they
+fail they get back up and say "I'll try harder next time".
+
+What is surrender? 
+
+''(digraph "Surrender" ''
+
+Sex -> Throne
+Business -> Throne
+Relationships -> Throne
+Financial -> Throne
+Family -> Throne
+
+Throne -> HS [label="Filled"]
+Throne -> Self [label="OOF"]
+
+'')''
+
+Isaiah 53:6. Because we have this old nature we are all rebels at heart.
+
+The problem with a living sacrifice is that it keeps crawling off the alter.
+
+The start of filling is often brokenness.
+
+The danger is that there is some formula for filling - but the reality is that
+the filling comes when we live a day-by-day surrendered life.
+
+'']
