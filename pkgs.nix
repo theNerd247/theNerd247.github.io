cf0ef10ba595d9722538d4d9716b17672701fd73
theNerd247 <noah.harvey247 gm>
2020-11-04 00:41:34 -0500

    updates conix

--- pkgs.nix
+++ pkgs.nix
@@ -2,7 +2,7 @@ import <nixpkgs>
 { overlays = import (builtins.fetchGit
     { url = "https://github.com/theNerd247/conix.git";
       ref = "master";
-      rev = "323075a852742c5078157e0f99818674abce09d5";
+      rev = "b5e44dd5c9f8df80c80105d2f276698fea287d89";
     })
     { extensions = 
         [ (import ./time.nix)
