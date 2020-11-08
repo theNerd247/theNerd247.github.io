d01325290317d977b2e95288500b79cd67a68520
theNerd247 <noah.harvey247 gm>
2020-11-08 11:54:54 -0500

    adds sermons/walkingByTheSpirit.nix

---
+++ sermons/walkingByTheSpirit.nix
@@ -0,0 +1,112 @@
+conix: with conix;
+
+"# "{ title = "Walking By The Spirit"; }''
+
+# Intro
+
+What do we do when we're offended? Any form of hurt is grief.
+There is a sense of loss, they feel rejected and thus a loss
+to their self worth or percieved esteem. Have you thought
+that maybe we grieve God the Holy Spirit?
+
+That's what the Bible says. There are things that we do that grieves
+Him and thus prevents His power from manifesting itself in our lives.
+
+Remember that God wants to hear His heart beating strongly in our lives.
+
+Many Christians live powerless lives in regards to the H.S:
+
+  1. They don't know the provision of power that God has layed at their
+  finger tips. We forget that God has put His very nature in us or we
+  don't understand what that means.
+  1. They never learn how to activate the Spirit's power in their lives.
+  They have a Christian life with no heart in it.
+  1. They see the Christian life as being one emotional high as the next.
+  The H.S does bring emotions. It is the _fruit_ (not "fruits") of the spirit.
+  Gal 5:22-23 gives the fruit: virtues that enable one to live an exceptional
+  and extraordinary life.
+
+''(digraph "factFaithFeelings" ''
+
+Feelings -> Faith [label="follows"]
+Faith -> Fact [label="fact"]
+
+'')''
+
+We should not base our Christian life on feelings - but fact.
+
+Eph 5:18
+
+The secret to the Christian life is being controlled and filled with the
+spirit.
+
+filled
+: plerao
+: speaks of being controlled continuously.
+: like wind in a sailboat
+
+If God commands this then why do I need it? Just like how wine controls a drunk
+man so is how one who is filled with the Spirit is out of his natural
+character; he is controlled by the H.S. Gal 5:22 the fruit produced in us must
+come from a power source.
+
+The Christian life isn't difficult, but impossible. You cannot live it in the
+flesh. So how do you activate that power source?
+
+  1. Enter in a relationship with God
+  1. Embrace total dependence on God. It begins with an overwhelming
+  realization that we are helpless. What keeps us from coming to that
+  place? Pride. We try and do it on our own.
+  1. Recognize that God uses brokeness to bring about surrender and wipe
+  our pride out the door.
+
+What impedes the Spirit's work in our lives? 
+
+Eph 4:30
+
+The word grieve is a love word. _You cannot grieve someone that doesn't love
+you_.
+
+# Recognize the Spirit's feelings. Eph 4:30, 
+
+Eph 1:13
+
+sealed
+: once; for all.
+: a wax seal used to declare: a transaction was finished (like a notary),
+ownership, create security.
+: Our salvation is complete, God owns us, and we are secure in Him.
+
+If you have trusted Christ by faith you cannot lose your salvation but you
+can block His fellowship.
+
+If you find yourself an unhappy Christian then it could be that something
+within you is grieving the Holy Spirit. When He is pleased with your walk then
+He is released within your life.
+
+What causes one to grieve the Holy Spirit?
+
+Eph 4:22
+
+lay aside
+: apotithemi
+: to put away from
+: aorist tense - put away from you once and for all. 
+: Put it away from you and don't pick it back up!
+: The idea is of taking off dirty clothes and putting them in the hamper.
+
+# Guard your hearts from the acts of the flesh
+
+Romans 6:6. Stop being slaves today. When we sin, God doesn't change; we have
+moved.
+
+
+Things that grieve the Holy Spirit (applied to the church at Ephesus):
+
+1. Dishonesty. Our old father was Satan (John 8:44)
+1. Cheating / Stealing. We as believers should work as unto the Lord.
+1. Rotten speech. Gossip, being critical, judging someone
+1. Malice, wrath, slander. All forms of anger manifested in a sinful way.
+
+Remember we could hurt the feelings of Christ and the Holy Spirit.
+''
