2dc16a1646ccd846e7647b383a8d0209904fa461
theNerd247 <noah.harvey247 gm>
2020-11-17 15:39:55 -0500

    cleans up runJS to use new syntax

--- runJs.nix
+++ runJs.nix
@@ -1,21 +1,25 @@
-conix: { 
-  docs.jsSnippet.docstr = ''
+conix: with conix; module (_: [])
+{
+  jsSnippet = expr 
+  "Name -> JsCode -> Module" 
+  ''
     Runs some javascript code and produces code blocks - one for the code
     and the other with the evaluated code (using nodeJs). The resulting
     module is nested under the given name.
-  '';
-  docs.jsSnippet.type = "Name -> JsCode -> Module";
-  lib.jsSnippet = name: code:
-    let
-      nodejs = conix.pkgs.nodejs;
-
-      codeFile = conix.pkgs.writeText "${name}.js" code;
-
-      outFile = conix.pkgs.runCommandLocal "${name}-stdout" { buildInputs = [ nodejs ]; } ''
-        ${nodejs}/bin/node ${codeFile} | tee $out
-      '';
-    in
-      conix.lib.set name 
-        (conix.lib.snippet "javascript" code "> ${builtins.readFile outFile}");
-
+  ''
+  (name: x:
+    code "javascript" (file 
+      (codeText:
+        let
+          jsFile = pkgs.writeText "${name}.js" codeText;
+          
+          nodeStdOut = pkgs.runCommandLocal "${name}-stdout" { buildInputs = [ pkgs.nodejs ]; } ''
+            ${pkgs.nodejs}/bin/node ${jsFile} | tee $out
+          '';
+        in
+          builtins.seq nodeStdOut jsFile
+      ) 
+      x
+    )
+    ;
 }
