9b64671aadf290a9da6317bb4ee4275d7d933eee
theNerd247 <noah.harvey247 gm>
2020-11-19 12:27:18 -0500

    adds notes

---
+++ notes/gradSchoolPurposeStatement.nix
@@ -0,0 +1,38 @@
+conix: with conix; ''
+
+# Interests 
+
+* Practical category theory for writing APIs and writing software (modelling?)
+* Type systems?
+* Writing interpreters
+* Writing DSLs for domains
+* Invertable Parsing
+* Semantics?
+* Programming language implementation with category theory as semantics
+* Lenses
+
+What do I want to learn?
+
+  * Deeper applications of category theory and writing sound APIs
+  * How writing a language works
+
+How can I help the field grow?
+
+  * A character based answer? -> curious, self-driven, etc.?
+  * Current research I've done -> I haven't formally done any research...I'll
+  need to get creative here...
+
+What have I done to show I enjoy research?
+
+  * Reading types and programming languages
+  * Category Theory for programmers
+  * other research papers
+
+What other things have I done outside of STEM to show self-drive / community
+influence?
+
+  * Volunteering at my Church
+  * Involvement with ministries with college students
+  * Mentoring college students
+
+''
