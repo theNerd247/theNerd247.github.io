3fd592cf9043aa7fec3043cbf46cf437f24cd558
theNerd247 <noah.harvey247 gm>
2020-11-20 18:27:36 -0500

    [wip] notes/gradSchoolPurposeStatement.nix

--- notes/gradSchoolPurposeStatement.nix
+++ notes/gradSchoolPurposeStatement.nix
@@ -1,38 +1,146 @@
 conix: with conix; ''
 
-# Interests 
+I am applying to Carnegie Mellon's Doctoral Computer Science program as my next
+step towards a career in academics. I intend to study the foundations of
+programming languages and how category theory can be used in the development
+of programming languages.
 
-* Practical category theory for writing APIs and writing software (modelling?)
-* Type systems?
-* Writing interpreters
-* Writing DSLs for domains
-* Invertable Parsing
-* Semantics?
-* Programming language implementation with category theory as semantics
-* Lenses
+Since I started programming I have been interested in what makes programs well
+organized, bug free, and idiomatic. I believe that these three principals are
+tightly coupled. I.e. bug free code tends to be well organized and contain
+idiomatic APIs. Further, if a program has a mathematical model as its
+foundation then it tends towards achieving the previously stated goals. I
+believe that category theory gives a functional programmer a great tool for
+reasoning about programs and constructing models to solve their problems with.
 
-What do I want to learn?
+While in college I discovered Haskell and functional programming.
+Inevitably I discovered Category Theory and the field of modelling programs
+mathematically. I immediately started applying basic concepts like functors,
+monads, semigroups, and monoids to my daily programming jobs in OOP languages.
 
-  * Deeper applications of category theory and writing sound APIs
-  * How writing a language works
+By the end of college I was introduced to lambda calculus. I then became
+fascinated with how functional programming languages were written and their
+typing systems. After learning how to create eDSLs I began to see how programs
+could be well written if eDSLS were used (example given?). From there I found
+that Category Theory could be used as a formal reasoning system for writing
+programs that were well defined in their behavior and thus have less bugs, and
+naturally the code was well organized.
 
-How can I help the field grow?
+Over the last 3 years I have been reading research papers, Category Theory For
+Programmers, and Benjamin Pierce's _Types and Programming Languages_. I have
+also started writing an eDSL for bringing high-order logic to markdown. I used
+categorical constructions as the basis for implementing the language. I have
+also began work on a denotational semantics for modelling a language for
+personal finances.
 
-  * A character based answer? -> curious, self-driven, etc.?
-  * Current research I've done -> I haven't formally done any research...I'll
-  need to get creative here...
+I spent a year teaching at a software engineering boot camp where I taught
+introduction to web development, algorithms, and datastructures. While there I
+discovered my passion for discovering new things and sharing them with others.
+I learned the skill of teaching adults using non-traditional methods and
+mentoring. I investigated better methods for writing ReactJS code.
 
-What have I done to show I enjoy research?
-
-  * Reading types and programming languages
-  * Category Theory for programmers
-  * other research papers
-
-What other things have I done outside of STEM to show self-drive / community
-influence?
-
-  * Volunteering at my Church
-  * Involvement with ministries with college students
-  * Mentoring college students
+I have also made continuous efforts to be mentored by others and privately
+mentor others.
 
 ''
+
+# * What am I interested in?
+# 
+#   - Language Design
+# 
+#     - Quality code is idiomatic code 
+#     - Idiomatic code is a language (no matter how small)
+#     - Small, composable DSLs makes it easier to write flexible code
+# 
+#   - Mathematical Models
+# 
+#   - What led me to category theory?
+# 
+#     - Learning Haskell 
+#     - Category Theory as a math background for modeling and reasoning about programs.
+#     - Using category theory to drive design of APIs
+# 
+# - Background in robotics?
+# 
+#   - Category Theory
+#     
+#     - how category theory applies to programming and the development of
+#       programming languages 
+# 
+#     - generally - mathematical models for writing programs.
+# 
+#     - Why interest in mathematical models?
+# 
+#       - concise and idiomatic APis
+#       - ensures correct behavior of programs
+#       - writing programs that are easily refactored. 
+# 
+#   * Why do I think they are important?
+# 
+#   * Why am I well suited to pursue these interests? 
+# 
+#     * What evidence of me having the capacity of performing research?
+#     * What efforts have I already taken to study the areas I'm interested in?
+#     * Do I have an interest in sharing my thoughts with others and teaching?
+# 
+# * Applications of Category Theory to the development of programming languages
+# and programs
+# * Writing well documented code. 
+# * Teaching and mentoring others 
+# 
+# 
+#  * Interested in writing well organized code and idiomatic APIs
+#  * Category theory has helped in modelling these APIs
+#  * Creating accurate models is important for implementing correct,
+#    refactorable, and well documented code.
+#  * I am interested in how category theory can be used as a domain for
+#  modelling programming languages and the construction of APIs and DSLs.
+#
+#    * Read Category Theory For Programmers
+#
+#  Why am I interested in this?
+#
+#  What journey have I taken to do this?
+#
+#    * Read Category Theory For Programmers
+#    * 
+#
+# # Interests 
+# 
+# * Practical category theory for writing APIs and writing software (modelling?)
+# * Type systems?
+# * Writing interpreters
+# * Writing DSLs for domains
+# * Invertable Parsing
+# * Semantics?
+# * Programming language implementation with category theory as semantics
+# * Lenses
+#
+# What do I want to learn?
+#
+#  * Deeper applications of category theory and writing sound APIs
+#  * How writing a language works
+#
+# How can I help the field grow?
+#
+#  * A character based answer? -> curious, self-driven, etc.?
+#  * Current research I've done -> I haven't formally done any research...I'll
+#  need to get creative here...
+#
+# What have I done to show I am self driven in research?
+#
+#  * Reading types and programming languages
+#  * Category Theory for programmers
+#  * other research papers
+#  * Aided as a TA in college - organized labs, taught intro to C and embedded
+#  programming and
+#
+#What other things have I done outside of STEM to show self-drive / community
+#influence?
+#
+#  * Volunteering at my Church
+#  * Involvement with ministries with college students
+#  * Mentoring college students
+#  * Travel to Europe
+
+
