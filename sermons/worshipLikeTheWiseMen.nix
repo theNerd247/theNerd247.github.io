c5af7ed5869e0c0617691bb630b2e458338f2b46
theNerd247 <noah.harvey247 gm>
2020-12-06 12:02:58 -0500

    adds sermons/worshipLikeTheWiseMen.nix

---
+++ sermons/worshipLikeTheWiseMen.nix
@@ -0,0 +1,111 @@
+conix: with conix; [''
+
+# ''{ title = "Worship Like The Wise Men"; }''
+
+# Intro
+
+Emily Post - the guru on social edicate. Today she would be appalled at
+the lack of manners. For example no one brings a gift when going to a
+dinner party. Thankfully we still bring gifts to birthday parties.
+
+So what about Christmas? What gift do we give to the king of kings?
+
+## Context
+
+Matthew 2 
+
+
+  - "they came from the east"
+
+from the east
+: from the rising of the sun 
+
+magi
+: has root in persian language
+: not a common word 
+: used as a designation for leaders/kings as being particular people
+: transliterated as magician, magnificant, and magnum. Each meaning "great".
+
+They were from the descendents of Shem. Practiced Zoroastrianism - a pre
+Islamic religion. Their god was monotheistic (called Mazda). How in the 
+world would they have interest in the Messiah?
+
+Remember Daniel and the Jews were taken into captivity by Babylon. The vision
+of the 4 kingdoms was given him where in the Medes and Persians would take
+over Babylon. Thus the Medes and Persians heard this teaching from the Jews.
+There were others - not just the Jews - that were looking for a Messiah.
+
+Spiritually these men worshiped Christ. They were pagan - however it is
+probable that they had some belief in Christ. The Magi used a star - which
+the magi loved to study - to draw them to the babe Christ. 
+
+The star was possibly God's shikinah glory; leading like the pillar of fire.
+
+They travelled a very far distance on camels. And brought extreme riches.
+
+Isaiah 60:4-7 (a partial picture of what was to come prior to the first coming of Christ).
+
+# Sacrifice As The First Gift
+
+Do we sacrifice for Christ? 
+
+> In love for my savior I am going to sacrifice for Christ. Whatever it means
+to give > up what is comfortable for me so that I am serving Him.
+
+This means that it's incumbent on us to say "God I'm yours". Do we really mean
+this? As you think about gifts don't forget about your sacrifice for Christ.
+
+> What can I get a King? I guess I can give Him myself.
+
+This means that you are ready to identify with Christ and say: "I am in Christ
+and Christ in me". 
+
+Micah 5:2. Intellectually speaking we are Christians because of the
+overwhelming veracity of Scripture. 
+
+I am not going to identify myself as an American or anything. I stand with Him!
+I will be counted as a Christian with Him in this world. Like a submarine we
+are not meant to live under deep water. There is immense pressure from the
+outside to conform to the rest of the world. But we have the H.S in us!
+
+Matthew 2:8
+
+Herod probably knew of the verse in Micah 5 however he was skeptical. So he
+sent the magi.
+
+# Sacrifice Popularity For Solidarity
+
+We live in a world were people are consumed  with wanting to be accepted.
+Many are trying to make Christianity acceptable. Today people say "we will
+not tolerate your Biblical ethics and standards". 
+
+Today we make abortion acceptable. Psalm 139 says so. 
+
+Be loving and graceful when with people but stand firm for the truth.
+
+# Sacrifice Idols for Real Treasures
+
+worship
+: to ascribe glory to the LORD and credit to His name
+: "kabod" 
+: gravitas, the importance, dignity. 
+: It is saying to the LORD "Your are the most important person in my life".
+
+We need to progressively be giving over different areas of our lives.
+
+idol 
+: something I will not give up to God
+
+In light of eternity this is just a short window.
+
+# Application
+
+> Isaac Wats:
+> When I survey the wondrous cross 
+> On which the prince of glory died
+> My richeest gain I count but loss
+> And pour contempt on all my pride
+> 
+> Were the whole realm of nature mine
+
+'']
