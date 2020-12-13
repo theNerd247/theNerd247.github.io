e03f4ffc92833cba677a232eb643f01c94c52c35
theNerd247 <noah.harvey247 gm>
2020-12-13 11:54:38 -0500

    adds sermons/servingLikeTheAngels.nix

---
+++ sermons/servingLikeTheAngels.nix
@@ -0,0 +1,137 @@
+conix: with conix; [''
+
+# ''{ title = "Serving Like The Angels - Luke 2:8-14"; }''
+
+## Intro
+
+A child saw a man stop cars from wrecking - possibly an angel?
+
+The only way we will understand life is by aligning our lives with the 
+perfect life. We can only be saved by having the righteousness (+R) of
+Christ.
+
+hypostatic union
+: Christ being fully God and fully man - the God + man savior
+: see Phil 2 - "He emptied Himself" thus giving up his independent use of His
+divine attributes.
+
+What are the Christmas presents should we bring to the King of Kings? 
+
+We don't think of Angels that often - especially during our daily lives.
+However, angels are ubiquitous throughout scripture - 214 in OT 186 in NT (300
+in total).
+
+### Context of Angels 
+
+1. They are a creation of God - just like man was created by God. Ps. ???
+
+1. They are invisible - spirits. They are not ghosts (demons impersonating
+people).  Computers have hardware + software (visible + invisible). Humans have
+visible (body) and invisible (soul + spirit). Angels can be thought of software
+only. They have the invisible (spirit) but not the hardware (body).
+
+1. They are made to bring credit to God. Col 1:16. They were created before the
+foundation of the world. 
+
+1. They were created holy and righteous (good angels that is. The bad ones are
+demons).
+
+1. They are more powerful than humans. They can transport through physical
+space instintaneously.
+
+## The Angels Tell Us About the Gift of Service
+
+Luke 2:8-14. To whom did the angels appear? Shepherds.
+
+Revelation 4:5
+
+The creatures before the throne - seraphim - were angels. 
+
+Rev 10:1 - this is an angel that is high up in God's pecking order.
+
+Hebrews 1:14 - the angels are ministring spirits meant to serve those who
+are to inherit salvation.
+
+These angels are going to stinky heards men to give great news. What does this
+teach us? They were willing to serve - though powerful - to do God's will.
+
+### We serve as a gift of selflessness 
+
+_Never be to big to serve others_. You are not more important to serve other
+people.
+
+People don't care how much you know until they know how much you care.
+
+Submit yourselves to one another - Eph 5.
+
+I care enough to submit to you and let you be a part of my life and me a part
+of yours.
+
+diakonia
+: to serve tables
+: rendered as "deacon" today.
+
+Remember how a menial task this must have been to come down and give this news
+to shepherds.
+
+Walt Disney was a genius. No one was referred to as "workers" they were "cast
+members". You're an actor and everyone picks up trash.
+
+Gal 6:10 - _serve others_. Focus on, this Christmas, on how you can serve
+Christ through a selflessness way. Think about some of the people around
+you and see who you can serve. How many times do we say "pray for me" vs "how 
+can I pray for you"? If God hates pride what do you think He thinks about
+selflessness?
+
+It's easy to get bogged down this season about COVID and presents and family
+meetings.
+
+## Serve as a Gift of Love
+
+Luke 2:10
+
+Did God need to use angels to bring this message? Couldn't He use his audible
+voice?  Yes, but He chose to enlist his servants the angels.
+
+If you want to love me then go serve these people.
+
+1 John 3:16
+
+You can't buy God anything. You can't improve the perfect triune God. v17 says
+that we serve God and get Him a gift by serving and sacrificing our lives
+towards those around us.
+
+Psalm 50 - if God had a need we wouldn't ask us to fill it. He creates needs 
+within people around us and then asks us to serve them.
+
+We slow God up - the angels too. Think of a runner who is a pro running with
+you.  You are slowing them down and they aren't getting any benefit from
+running with you.  God could snap His finger and all problems could be solved! 
+So why does He not do this? He is saying to each of us - "I want to give you the
+privilege by serving those who are my children".
+
+## Serve as A Gift of Worship
+
+Luke 2:13-14
+
+God is pleased only with those who have trusted Him; it is those people who
+have received peace.
+
+Romans 12:1 - this represents all of us. You surrender; crawl up on the alter,
+and give your life over to God. 
+
+* Pray for them
+* Meals to them
+* Be inconvenienced for them
+* Give them your ear
+
+I care about being the Good News to someone this season. Serve others!
+
+## Application
+
+* God help me to be the good news this Christmas - to understand that life
+isn't about me but others.
+* God help me to find those people who are in need and then to have a heart
+to serve Him.
+
+'']
