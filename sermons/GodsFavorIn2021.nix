f377c0e58bb054d2c091a0be325062c46a5ee47d
theNerd247 <noah.harvey247 gm>
2021-01-10 11:55:52 -0500

    adds sermons/GodsFavorIn2021.nix

---
+++ sermons/GodsFavorIn2021.nix
@@ -0,0 +1,174 @@
+conix: with conix; [''
+
+# ''{title = "God's plan in 2021";}''
+
+Exodus 33
+
+# Intro 
+
+Byron was told he was going to be the quarter back. He found favor in
+the eyes of his coach. Don't we want to hear that from God? If we are
+serious about walk with God we ought to desire the same.
+
+Psalm 5:12
+
+Who wouldn't want God's favor that acts as a shield around them.
+
+Be sure that you have God's favor for this new year.
+
+Exodus 33 unfolds how Moses received favor from God.
+
+## Context
+
+God says to Moses in ch 32:
+
+> I've had it; I'm ticked; I'm ready to blow away this people.
+
+and in ch 33:
+
+> I will not go with your people. Because if I do go I will become so
+> sick that I will destroy them. I will send an angel to drive out
+> the Caananites but I will not go.
+>
+> - (Exodus 33:2)
+
+
+When we live by the flesh, embrace sin, and greave the HS - God causes the
+conscious in that person will wear them down. God placed the tabernacle outside
+of the camp because He cannot have fellowship with sin. We see this here between
+God and Israel - a NT concept illustracted in the OT.
+
+Ex 33:12
+
+# The Desire Of Fellowship and Knowing God
+
+Reading between the lines this is a man (Moses) who desires God's favor. And in
+order to find favor he must know God in a personal way.
+
+know
+: yada
+: the Hiphil imperative - "cause me to know".
+
+_Is your passion "God I must know you" this year?_
+
+Paul said in Phil 3:10, 2 Pe 3:18 
+
+Psalm 103:7
+
+the "ways" were made known to Moses, but only the "acts" were made known to
+Israel.
+
+Again "know" here means a deep personal knowledge of God; "acts" refers to
+superficial knowledge. Moses knew God from face to face encounters.
+
+2 levels of knowledge: about the things of God, and who God is and why He does
+the things He does. Some know God only by the works that He does. How many know
+God personally? _Do you?_ The only way to know God is by:
+
+* An addiction to knowing God. To seek Him out through His Word.
+
+Incomprehensibility of God
+: God cannot be finally and completely understood
+: 1 Tim 6:16
+
+_How do we develop this relationship without physically seeing a person_?
+
+If we can completely know God then He is not God. The HS will teach you -
+He will open your eyes to knowing Him. 
+
+Don't just show me what you do, but why you do what you do; how you operate.
+God doesn't do this by osmosis. This isn't theological stuff - but practical.
+It will change you life.
+
+''(list [
+
+  "sovereign"
+
+  "perfect righteousness"
+
+  "omnicient"
+
+  "omnipresent"
+
+  "omnipotent"
+
+  "love"
+
+  "eternal life"
+
+  "unchanging"
+
+  "truthful"
+
+])''
+
+Isa 54:10
+
+Maybe you don't feel this? You feel God has abandonded you. What do you do?
+Return to the chart and see the character of God.
+
+Study and memorize the attributes of God.
+
+# Seeking God's Guidance in All Things 
+
+Ex 33:12
+
+You have not let me know who will go with me. Moses here is seeking God's
+guidance.
+
+Judges 6 - Gideon was wanting to be absolutely sure what God's guidance was.
+David was seeking out God's guidance (His Shepherd), as was Paul.
+
+Why do we need to seek God's guidance?
+
+1. It demonstrates dependence upon God - this becomes a lifelong process for
+me.
+1. It demonstrates obedience to God
+1. It demonstrates my recognition of a fallen nature that keeps me often from
+seeing things as God sees them. 
+
+We all need a healthy respect, awareness, and distrust of the old nature - for
+sometimes the very thing that feels right is not the thing we desire.
+
+`Your ways != my ways`
+
+Proverbs 3:5-6 Do not lean on your own understanding!
+
+How to listen to God by Charles Stanley (p46).
+
+Ever have the desire to see far ahead when you're stuck in traffic? If only
+there were a plane that you could radio to navigate through or out of the
+traffic? That is the way it is between us and God.
+
+
+# Seek God's Presence in All Things
+
+Ex 33:15-16
+
+presence
+: lit "show me your face"
+: the sense of being in the immediate presence of God
+
+you
+: is not plural but singular
+: 2nd person, masculine, singular
+
+Why does God say this? Because He cannot have fellowship with sin
+
+rest
+: to pause
+: to be left alone
+
+God's rest is a result of our trusting Him. Whatever your circumstance is 
+trust God - it's a moment by moment resting in God.
+
+# Application
+
+* Know that God wants a personal relationship with Him more than you want
+it. 
+
+* Know His presense has a common characteristic of rest, peace; pause.
+
+
+
+'']
