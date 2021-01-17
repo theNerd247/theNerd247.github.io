c19032c95d87b474863a8db5c22e1ceb430f9dfd
theNerd247 <noah.harvey247 gm>
2021-01-17 11:54:24 -0500

    adds sermons/experiencingThePowerOfTheHolySpirit.nix

---
+++ sermons/experiencingThePowerOfTheHolySpirit.nix
@@ -0,0 +1,181 @@
+conix: with conix; [
+
+''# ''{title = "Experiencing The Power of The Holy Spirit"; }''
+
+# Intro
+
+John 15
+
+Imagine that you're going to buy a birthday gift for a pre-schooler.
+You ask a toy-store cashier for advice and are taken to a toy truck
+aisle. The toy requires are required to have batteries - which are 
+included in the in truck.
+
+The child recieves the gift and begins to play with the toy as if it
+had no batteries. These are like Christians who live as if they never
+recieved the Holy Spirit. They are trying to live the Christian life
+on their own efforts; the live their life as if they were weaned on a
+dill pickle and eventually give up and walk away from their relationship
+with God.
+
+Eph 5:18 
+: this is one of the few passages that uses active tense when talking about the
+H.S. Normally the aorist tense is used. Filling is to be a continuous and involved
+process by both the person and the H.S.
+
+1 Cor 2:14 - 3:4
+
+note:
+
+  * spiritual man (spirit filled)
+  * fleshly / carnal man (not spirit filled)
+  * merely human (the unbeliever)
+
+E.g a "giving man" is one who is characterized by his giving. In likewise a
+spirit filled man is characterized by the working of the Holy Spirit; the
+fleshly man is characterized by the acts of the flesh.
+
+John 15 is in similar vain - to be continuously filled/controlled by the Holy
+Spirit.
+
+''(sideNote ''Be careful with complacency when studying scripture. God illuminates
+our hearts and fresh bread is awaiting us. God's word speaks - we come together
+to hear God speak. The job of the preacher is that of a waiter: to get the food to the
+table without messing up the food.'')''
+
+"I am"
+: speaks of the eternality of Jesus; His pre-existance of God that He existed
+before all time.
+: lit. "Ego eimi"
+: lit. "I I am" - akin to the OT statement "I am that I am". This is a
+declarative statement.
+
+"I am the true vine".  Jesus uses word pictures that are relavant to the men he
+is speaking to. The vine used to represent Israel.
+
+true
+: genuine, authentic, real
+: aletheinos
+
+abide
+: meno
+: to remain, stay in place
+: constative aorist 
+
+constative aorist
+: the totality of the action - your life style.
+
+"he who constantly abides in me". The picture is that in order for the branch
+to live it must stay connected to the Holy Spirit's power in your life.
+
+When Jesus asks "where are you staying" he uses the word "meno" which means to
+abide. 
+
+# Know that Christ Expects Us To Bear Fruit
+
+bear fruit
+: the result of abiding.  
+: see Gal 5:22 - 23 (note fruit is singular)
+
+Fruit of the Spirit:
+
+  * love
+  * joy
+  * peace
+  * patience
+  * kidness
+  * goodness
+  * faithfulness
+  * gentleness
+  * self-control
+
+_Why is this not only seeing others come to Christ?_
+
+
+Why does the fruit of the spirit not work for some? He doesn't expect us to
+_produce_ the fruit but to _bear_ the fruit. Our only verbal command is to
+abide - He is the one that produces the fruit in our lives.
+
+''(sideNote ''What's the difference between "produce" and "bear" fruit?'')''
+
+We get frustrated in our lives - and a major part of the problem is that we are
+trying to produce the fruit.
+
+Gal 2:20 we get burdened down because we get frustrated.
+
+Poodle says he is greater than a german shephard. The german shephard
+disagreed.  They made a contest to open the door. The german shephard uses all
+his might and after 3 minutes opens the door. The poodle walks up to the other
+door - whines and scratches and the owner opens the door. This is a picture
+between the believer running on his own strength and the one who is connected
+to the Master. The believer must rest and grow in his relationship with God. It
+is then that Christ changes his heart and mind and out of that produces the
+fruit in the believers life.
+
+# Understand the Christian life is impossible
+
+"Without me you can do _nothing_." Keep in mind that the disciples anticipated
+that something big was going to happen; they were fearful. Christ knew how
+dependent they were on Him. Christ was saying: "You need to be just as
+dependent on who I'm sending as you are on me today". He knew how wandering
+(like sheep) they were and would try and live by their own power. God knew 
+how self-powering we are. 
+
+The Holy Spirit is the divine sap giver. He is the abiding abiding presence of 
+God in your life. "It is no longer I who lives but Christ who lives within me".
+In Romans 7 Paul wasn't saying that He was falling into gross sin but that he
+was having a hard time living out the Christian life. 
+
+''(sideNote ''For accountability the emphasis should be placed on our abiding
+in Christ - not in avoiding the sin'')''
+
+Imagine one who has no musical ability and tries out for the lead role in a
+musical.  The director mentions that she is tone-deaf and confronts her. She
+says "Oh please let me have it anyway. I'll practice really hard - everyday. I
+know I don't deserve it but let me at a chance". She doesn't realize that she
+doesn't have the natural talent it's impossible for her to.
+
+''(digraph ''
+
+"Abiding in Christ" -> "Fruit of the Spirit"
+"Abiding in Christ" -> "Avoiding Sin"
+"Abiding in Christ" -> "Witnessing"
+"Abiding in Christ" -> "Helping the poor"
+"Abiding in Christ" -> "Healing from damaged emotions"
+"Abiding in Christ" -> "Reconciliation"
+
+'')''
+
+Sin is _not_ the root of the problem but the symptom. We are living out our
+lives independent of our relationship with God.  We have to have faith that 
+when we abide in Christ and trust that He will produce the fruit in us
+
+# Realize that No On abides without faith
+
+Going back to the singer analogy. Imagine that a doctor says "we are going
+to replace your vocal chords to make you sing like the best of them". That's
+what God says to us. He is going to give us batteries that are so powerful
+that will change your life and produce unbelievable power. But you have to
+get out of the way and just rest in me.
+
+Phil 4:9
+
+can do
+: In all things I am continuing to be strong in Him who is continuing to
+empower me. 
+: Gal 2:20
+
+Spirit filled life. If you never learn to abide in Christ you will never have
+sufficient self-control to resist temptation. 
+
+# How do I have a Spirit filled life?
+
+1. Confess your sins. You cannot be empowered if you have _unconfessed_ sin
+(_not lack of  sin_).
+1. Surrender your life to the vine
+1. By faith believe that you're connected to the vine and thus, the vine will
+give you life and power.
+
+_Trust me! And watch me work!_
+
+'']
