0e0f4c62fa26b49acb8ccf728067d95cba065d94
theNerd247 <noah.harvey247 gm>
2021-01-24 11:54:34 -0500

    adds sermons/DivineNightGoggles.nix

---
+++ sermons/DivineNightGoggles.nix
@@ -0,0 +1,142 @@
+conix: with conix; [ ''
+
+'' { title = "Divine Night Goggles - 1 Cor 2:6-14"; }''
+
+# Intro
+
+Pastor got to sit in an M1 tank - night vision goggles. With
+these on one can see in the dark. When it comes to reading
+God's word we often feel like we're in the dark. Emotionally
+we're not recieving anything, mentally we are confused. 
+
+However, the HS acts as our night goggles - giving us the ability
+to see what we cannot see naturally. He holds the ministry
+of _illumination_.
+
+# Historical Background
+
+Paul is commanding the believers to stop being enamored by the Greek
+philosphers of the day. Emphasis was placed on how to speak eliquently.  For
+example the sophists were philosophers who would wax eliquently about the
+"logos". Essentially these were preachers who were watering down the Word of
+God in order to make it more pallatable. Remember the Christians did not come
+from the upper ends of the intellectual world.
+
+# Man Requires the Help of the Holy Spirit To Understand The Things of God
+
+1 Cor 1:26-29
+
+sophia
+: hebrew for wisdom
+: used 7 times, 4 for wisdom of man, 3 times for wisdom of God in 2:6 it is
+used regarding the wisdom of God taught to the mature
+
+Infants of Christ vs mature in Christ
+
+1 Cor 3:1
+
+_True wisdom cannot be discovered by human thinking_. It's impossible for a
+lesser creature to understand that of a greater. E.g. a flea doesn't understand
+the mind of a man. _Men cannot understand God_.
+
+> If this is true then why do we try and get to know God.
+>
+> Does this mean we are allowed to make truth claims about God?
+>
+> How does this affect our hermineutics that is how can one know what is true
+>  about God if we are require the HS.
+
+1 Cor 2:9
+
+### Philosophies of the Day:
+
+Imperism 
+: knowledge can only come out of actual experience
+
+rationalism
+: belief that the exercise of reason provides the basis for knowledge
+: if you cannot reason it then it must not be true
+: disregards authority, divine revelation
+
+gnosticism
+: Matthew 11:25
+
+appraised
+: discernable 
+: Influenced and guided by the Holy Spirit.
+
+Watching tv on a 19" screen verses HD - but the 19" is connected to satalite -
+and you're not. You may have high ability of human reasoning but without the 
+HS you cannot understand the things of God.
+
+1 Cor 2:10 - no person can know another person better than they know
+themselves. Our deep thoughts are only known to ourselves. In a similar
+way only the spirit of God can fully know the thoughts of God. I.e it takes
+God within us to understand God the Father.
+
+# The HS Reveals Divine Wisdom and Insight
+
+illumination
+: enlightenment
+
+It causes the words on the page to bleed through and give us insight both into
+what the passage means and also how it applies to our mind.
+
+oida
+: to know fully - both intellectually and spiritually
+
+the HS goes deep thought fishing. He pulls out the deeper truths of God. Only
+the Spirit filled man can do this - he must be walking in the flesh.
+
+Examples:
+
+  * something new jumps off the page
+  * a perfect passage for our circumstances
+
+John 14:26, 16:13
+
+God's word promises that God's spirit will guide us into truth
+
+Simple concepts that God has on the surface it is complicated and difficult to
+understand. But then the HS comes and shows us how simple it is.
+
+1 Jn 2:20 - you continue to have annointing
+1 Jn 2:27 - the aorist tense we have already received the annointing.
+
+This does not refer to a speaker who gives a dynamic message.
+
+The annointing  is the HS: every Christian has the spriritual capacity to
+understand the deeper things of God when we are walking by the Spirit. We all
+have the same capacity to have intimacy with God. This is not only for the 
+super Christians to hear from God.
+
+Romans 11:33
+
+1 Cor 2:16 - we have the mind of Christ: the Word and Spirit of God.
+
+> "combines the spiritual things with those who are spiritual"
+>
+> - Philips Translation
+
+2 Kings 6:15,16
+
+1 Cor 2:15
+
+appraises
+: krino
+: to separate; discern; divide
+
+No one can know him but God! It is by the HS that believers obtain discernment.
+Discernment vs not is like a man who is color blind. 
+
+# Application
+
+* Cleanse yourself - come clean. When you have sin in your life you cannot see
+the deeper things of God
+* Set yourself before God with a heart of obedience; have the desire to hear
+from God 
+* Diligently seek the mind of God through His word
+
+See Charles Stanely - being empowered by the HS.
+
+'']
