067f526c0a5252e53cb2f96e46930a7dfb2d99ba
theNerd247 <noah.harvey247 gm>
2021-02-07 12:01:22 -0500

    adds sermons/youAndTheHolySpirit.nix

---
+++ sermons/youAndTheHolySpirit.nix
@@ -0,0 +1,101 @@
+conix: with conix; [
+
+''# ''{title = "You And The Holy Spirit"; }''
+
+# The Assurance In Salvation
+
+I have a deep fear of death and I'm amazed at how well you can sleep.  There
+was a lack of assurance of salvation - often a person's feelings play into
+this. 
+
+''(quote ''
+No one had need to ask if they had recieved the Holy Spirit. Fire is evident;
+so is power.
+''
+
+"Samuel Chadwick"
+)''
+
+''{p = "Romans 8";}''
+
+There is therefore no no no condemnation from the love of God.
+
+''(r data.p)'':15 those who are loved by God are sons of God. Only those who
+are led by the spirit. 
+
+He can only make claim to what he has done; not what God has done. A Christian
+boasts in what God has done. The sons of God are secure in Christ - even if
+they are not as obedient as they should be. This does not mean they will feel
+like it. One who neglects obedience, study, and/or fellowship they will most
+always question their salvation. Fear is the enemy's way to prevent Christ from
+living out His life within you.
+
+How do we determine how he (the Holy Spirit) leads us?  Romans 8:13 
+
+  - He leads by giviing us victory over the flesh.
+  - If you are becoming more and more like Christ.
+  - You lose the appetite for fulfilling the sinful nature.
+  - When I see I am moving from a self-directed; spirit directed life I am a
+  child of God
+  - By freeing us from the spirit of slavery that will almost always lead to a
+  spirit of fear.
+
+No matter how much we mask our unbelieving reality the non-christian is
+constantly living in fear. When you give in 
+
+''(code "" ''
+  Guilt -> Fear (Of judgement, of God)
+'')''
+
+Even the believer who gives into sin: we blow it and blow it over and over
+until we finally start asking: "where is God in all of this?".
+
+
+Hebrews 2:14-18, 2 Tim 1:7, 1 John 4:15-18, Galatians 4:1-7
+
+If we are saved and mature in Christ then we do not live by fear.
+
+The most miserable person on earth is the carnal Christian.
+
+The law was a task master leading one to become a believer.
+
+adoption
+: a pronouncement of becoming an adult with all the rights of privileges.
+: being placed as a son within a family
+
+patria potestas
+: the roman father had the complete authority over his family
+
+You as a child of God should not be fearful but confident - and not timid. You
+have all the rights of the Father. You are partners with Christ (business
+partners?) and have the ability to withdraw from the divine bank without limit
+whether of frequency or amount. We often fail to recognize the lofy position
+we have in Christ - not in arrogance but with confidence in Christ.
+
+John 1:12
+
+right
+: authority, power
+: exousia
+
+When you have this understanding you will address Him as "Daddy" who desires
+the best for you. The Spirit Himself testifies with our Spirit. Romans 8:17
+
+There had to be seven witnesses when an adoption occured. The Spirit of God
+bears witness to us. The sign of a son of God is someone who is an heir of God
+the Father and joint heir with Christ. A child has a potential of inheriting
+something. A Roman father would never refer to a son as his son until he
+reached the age of maturity; before then he was a child. This is the picture
+shown here.
+
+An heir
+: one who recieves part of the division of the inheritance. 
+
+A joint heir
+: one who recieves the whole of the shared inheritance.
+: a co-owner of the inheritance
+
+What role does the Holy Spirit play in our assurance? He bears witness to us
+that we are sons of the living God.
+
+'']
