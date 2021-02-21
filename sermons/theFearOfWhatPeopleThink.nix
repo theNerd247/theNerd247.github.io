0bd59f5d4b59acc39c6f1355baf9ad2ebcb2d0d0
theNerd247 <noah.harvey247 gm>
2021-02-21 12:02:20 -0500

    adds sermons/theFearOfWhatPeopleThink.nix

---
+++ sermons/theFearOfWhatPeopleThink.nix
@@ -0,0 +1,126 @@
+conix: with conix; [
+
+"# "{title = "The Fear Of What People Think - Luke 12";}''
+
+# Intro 
+
+Fear and Anxiety plays an important role in other people's
+lives.
+
+Fear paralyzes. A person cannot become all that God has planned for them
+to become if they live by their fear of other people. There's a fasade
+when we live this way. We live in a way that makes them think what we
+want them to think of us.
+
+How do I combat being a phony? How do I live without fear of other people.
+
+mirad
+: myrios
+: ten thousands
+
+Speaking of the pharisees Jesus said that there's no way to cover up the stench
+of a rotten egg.
+
+hypocrocy
+: to act or judge behind the mask
+: hupocrino 
+
+crino
+: to judge
+
+why does he use the word leaven? It refers to yeast. Before baking the leaven
+was set in a dark cool place that allowed the yeast to permiate the dough. To
+the Jews it always symoblized evil. Here Jesus uses the same symoblism to state
+that the hypocrosy of the pharisees is evil. See Galations where Paul uses this
+to speak of Jewish Christians still trying to live by the law.
+
+
+Types of Pharisees:
+
+1. Shoulder - wears his merrits on his shoulder for all to see
+1. Wait a little - hang around a little to gather a crowd to show off
+1. Bleeding - he closes his eyes and runs into walls to avoid lust
+1. Painted - advertises his holiness to avoid touching unclean things. "Don't
+touch me I'm holy!"
+
+3/4 in steal rotted and only 39 coats of paint remained. Be careful to not
+paint the outside. The art of concealment.
+
+It's typically not loud or brash - but subtle, and insidious. It's incremental
+and quiet.
+
+# Resist The Lure of Hypocrisy
+
+Run from the lure to not be honest with people. There's a gap between what we
+say and what we are. There's typically a gap between what you know and what you
+do. We are talking about the people who consciously hide who they are and how
+they feel. Take the cultural Christian: they fake reading their Bible and
+worship but there's no heart or passion.
+
+Jesus did not waste warnings: he only had 3 years of ministry. Whenever He said
+beware he was serious! Hypocrisy is subtle and real. It will creep in if given 
+the chance.
+
+Take Peter for example in Galatians 2:11. Gal 1:10 "Am I still trying to please
+man?! If I were trying to please man I would not be a servant of Christ!"
+
+We are not called to live for the audience of people. We are to order our lives
+to the audience of 1: God the Father.
+
+Everything which is hidden will be brought to the light:
+
+  * Luke 12:2-3
+  * Ecc 12:14 
+  * Romans 2:16, 
+  * 1 Cor 4:5
+
+# Be Cognizant Of Your Eternal Accountability
+
+John 5:27 
+
+At the Great White Throne. All unbelievers will be judged for their rejection
+of Jesus Christ. 
+
+The Behma seat is where every believer will stand before Christ who will give
+the final review of our lives. It may be "well done" for other he may say
+"there was so much I had for your life". 
+
+
+There won't be tears in heaven but there will be dissappointment. 1 Cor 3:12.
+
+Men do not fear God because they fear man too much.
+
+Luke 12:2-3 if what I say in private will be revealed then I need to have my heart
+cleaned.
+
+# Do Everything With Transparent Integrity
+
+integrity
+: Do what is right when no one is looking
+
+1 Tim 1:5
+
+sincere
+: unhpycritical faith
+
+Timothy struggled - perhaps - with fear of people. His love comes from a pure
+heart and a clean conscience.
+
+1 Pet 1:22.
+
+God doesn't want us to live a perfect life -  but an authentic life
+
+Phil 4: "Let your progress be made known to all men."
+
+# Application
+
+  * There must be an awareness to the temptation to be authentic. "My Father
+  loves me with Grace; imperfect as I am. He walks with me!"
+  * Reject the fear! "Why am I fearing?"
+  * Determine to live out an authentic lifestyle.
+
+
+''
+
+
+]
