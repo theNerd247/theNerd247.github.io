8cb56c1cd06675c0d8626cace297c7f486a8eb3c
theNerd247 <noah.harvey247 gm>
2021-02-28 11:51:42 -0500

    adds sermons/confrontingOurFears.nix

---
+++ sermons/confrontingOurFears.nix
@@ -0,0 +1,97 @@
+conix: with conix; [ ''
+
+# ''{title = "Confronting Our Fears";}''
+
+# Intro
+
+> If you don't settle down I'm going to call UPS to come and get you!
+>
+> - A frustrated parent
+
+Fear causes us to do irrational things.
+
+There are things we should fear
+
+Heb 4:1
+
+Why should I fear God? What is the fear of God?
+
+Luke 12
+
+# Context
+
+Jesus calls the Pharisees hypocrites because they live for the affirmation of
+men. You cannot see God's grace in your life if you are hiding behind a
+facade.
+
+# Fearing God is a Commandment
+
+This is troublesome for many because they see God as an angry person who
+is never satisfied.
+
+''(table ''["Misconception" "Reality"]
+[
+[ "Insensitive and uncaring" "Psalm 139 - kind and compassionate"]
+[ "stern and demanding" "Accepting and filled with joy and love"]
+[ "never satisfied with what I do" "He's patient and slow to anger"]
+[ "controlling and manipulative" "Full of grace and mercy"]
+[ "condemning and unforgiving" "Tenderhearted and forgiving"]
+]
+)''
+
+Notice in Luke 12:4 Jesus says "My friends" because he loves us and treats us
+as a friend. He is not intending to hurt us directly.
+
+Eph 1:18 - hope (confidence) of His calling, glory of His inheritance in the
+saints! I - the believer - is viewed as God's inheritance. 
+
+Proverbs 9:10 
+
+Fear - beginning of wisdom. In order to gain wisdom I have to step over the
+threshold of fearing God.
+
+Proverbs 10:27
+
+Proverbs 26:27
+
+Proverbs 14:26-27
+
+Proverbs 19:23
+
+Proverbs 22:4
+
+Proverbs 31:30
+
+_Fear is not to be your uniform emotional response to God_
+
+If every time I think of God I have this petrified fear then I would have a
+distorted image of God.
+
+## What is The Fear Of God?
+
+Fear is fear - it's a bad feeling.
+
+Why should I feel this feeling? It is the proper response when you come into
+contact with someone who has the unilateral power to bring negative affects
+into your life.
+
+Luke 12:4-5 We are to fear God because He is in control of our eternal destiny.
+
+Psalm 47:2
+Psalm 76:9
+
+## Fear God Because He is In Control Of Our Daily Lives; Even Breath
+
+Luke 12:5 - God has killed and can take us home after.
+
+Hebrews 12:5,10
+
+If God doesn't discipline you then you are not His son. 
+
+# Application
+
+The fear of God is not only for honoring got but is for our benefit
+
+See to have a balance understanding of the fear of God
+
+'' ]
